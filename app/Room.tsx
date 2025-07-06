1ca5c36bf86e50cf6faaf24b36cf91364d3e270a
Vinhn1 <vinhnguyen04275@gmail.com>
2025-07-06 10:14:02 +0700

    Xây dựng cấu trúc Project Và thêm tính năng cộng tác Realtime ở LiveBlocks

---
+++ app/Room.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+
+export function Room({ children }: { children: ReactNode }) {
+  return (
+    <LiveblocksProvider
+      publicApiKey={
+        "pk_dev_Zz4E5c0SpAPER_cI0I5fcLDM1mX5MwgXzlETzwXPfGFk38l_LKRsxr9wW-T-EbYu"
+      }
+    >
+      <RoomProvider id="my-room">
+        <ClientSideSuspense fallback={<div>Loading…</div>}>
+          {children}
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}
