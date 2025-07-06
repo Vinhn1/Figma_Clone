bb4fb5af285d93f3023ec7b827002af5d5c9f6ac
Vinhn1 <vinhnguyen04275@gmail.com>
2025-07-06 15:48:10 +0700

    fix các bug tài nguyên của project

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { Room } from "./Room";
+
 
 export default function Page() {
   return (
-    <Room>
+    <>
       <h1 className="text-red-500 text-2xl font-bold">Hello word</h1>
-    </Room>
+    </>
   );
 }
